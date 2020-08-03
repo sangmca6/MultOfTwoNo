86b641f21be28d5aa47d9c863cb143d22360ed01
sangeetha <Sangeetha@DESKTOP-BT8EDFF>
2020-08-03 20:29:04 +0530

    second commit

---
+++ src/MultiplicationOfTwoNo.java
@@ -0,0 +1,13 @@
+
+public class MultiplicationOfTwoNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int s=2,b=3,mult=0;
+		mult=s*b;
+		System.out.println(mult);
+		
+
+	}
+
+}
